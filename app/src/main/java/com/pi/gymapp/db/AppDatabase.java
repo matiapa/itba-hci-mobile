011c6dac3bf09dfb50f6fadf1e0b3a1f28c48023
cadiastands <jsackmann@itba.edu.ar>
2020-11-17 22:43:58 -0300

    hotfix

--- app/src/main/java/com/pi/gymapp/db/AppDatabase.java
+++ app/src/main/java/com/pi/gymapp/db/AppDatabase.java
@@ -6,8 +6,10 @@ import androidx.room.RoomDatabase;
 import com.pi.gymapp.db.dao.RoutineDao;
 import com.pi.gymapp.db.dao.UserDao;
 import com.pi.gymapp.db.entity.RoutineEntity;
+import com.pi.gymapp.db.entity.UserEntity;
 
-@Database(entities = {RoutineEntity.class}, version = 1)
+@Database(entities = {RoutineEntity.class,UserEntity.class}, version = 1)
+
 public abstract class AppDatabase extends RoomDatabase {
 
     public abstract RoutineDao routineDao();
