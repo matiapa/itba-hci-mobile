1d02edd3c33ff709e79ac39327d6264dbd41d37a
borrangie <aborracci@itba.edu.ar>
2020-11-21 02:29:37 -0300

    Adding darkmode button

--- app/src/main/java/com/pi/gymapp/ui/MainActivity.java
+++ app/src/main/java/com/pi/gymapp/ui/MainActivity.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -44,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
         ApiUserService userService = ApiClient.create(this, ApiUserService.class);
 
         AppPreferences appPreferences = new AppPreferences(getApplicationContext());
-        if (appPreferences.getAuthToken()!=null)
+        if (appPreferences.getAuthToken() != null)
             userService.getCurrentUser().observe(this, r -> {
                 if (r.getError() != null) {
 
@@ -75,7 +76,7 @@ public class MainActivity extends AppCompatActivity {
         Intent mainIntent = getIntent();
         Uri data = mainIntent.getData();
 
-        if (data!=null && data.getLastPathSegment()!=null) {
+        if (data != null && data.getLastPathSegment() != null) {
             RoutinesExploreFragmentDirections.ActionNavHomeToRoutineDetailFragment navAction =
                     RoutinesExploreFragmentDirections.actionNavHomeToRoutineDetailFragment(
                             Integer.parseInt(data.getLastPathSegment())
@@ -86,7 +87,7 @@ public class MainActivity extends AppCompatActivity {
 
         binding.navView.getMenu().findItem(R.id.nav_rate_us).setOnMenuItemClickListener(menuItem -> {
             Intent rateIntent = new Intent(
-            "android.intent.action.VIEW", Uri.parse("https://play.google.com/store/apps")
+                    "android.intent.action.VIEW", Uri.parse("https://play.google.com/store/apps")
             );
 
             startActivity(rateIntent);
@@ -96,13 +97,13 @@ public class MainActivity extends AppCompatActivity {
 
         binding.navView.getMenu().findItem(R.id.nav_sign_out).setOnMenuItemClickListener(menuItem -> {
 
-            userService.logout().observe(this,r->{
-                if (r.getError()!= null){
-                    Log.d("UI","Logout failed");
+            userService.logout().observe(this, r -> {
+                if (r.getError() != null) {
+                    Log.d("UI", "Logout failed");
 
                     Toast.makeText(this, getResources().getString(R.string.unexpected_error),
-                        Toast.LENGTH_SHORT).show();
-                }else {
+                            Toast.LENGTH_SHORT).show();
+                } else {
                     AppPreferences preferences = new AppPreferences(this);
                     preferences.setAuthToken(null);
 
@@ -160,23 +161,20 @@ public class MainActivity extends AppCompatActivity {
             case R.id.dark_mode:
                 isChecked = !item.isChecked();
                 item.setChecked(isChecked);
-                AppCompatDelegate.setDefaultNightMode(id);
 
-                /*
                 int mode = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
                 if (mode == Configuration.UI_MODE_NIGHT_YES) {
-
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
                 }
                 else if (mode == Configuration.UI_MODE_NIGHT_NO) {
-
-                }*/
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                }
                 break;
             default:
         }
 
         return super.onOptionsItemSelected(item);
     }
-
 
     @Override
     public boolean onPrepareOptionsMenu(Menu menu) {
